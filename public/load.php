81b97f416304644c9b2027f5ee2a0597316e1d79
Jhan Mateo <jhanxtreme@gmail.com>
2014-10-08 17:24:43 -0700

    modified base path

--- public/load.php
+++ public/load.php
@@ -27,15 +27,10 @@ define('PUBLIC_BASE_PATH', APP_PATH);
 // define script extension
 define('EXT', '.php');
 
-// PAGE BASE PATH
+// BASE PATH
 $jx_root_path =  (explode('/', $_SERVER['PHP_SELF'])[1] == 'index.php') ? '' : DS.explode('/', $_SERVER['PHP_SELF'])[1];
 $jx_base_path = $_SERVER['REQUEST_SCHEME'].':'.DS.DS.$_SERVER['SERVER_NAME'].$jx_root_path;
-
-if(is_dir(ROOT.$jx_root_path)){
-	define('BASE_PATH', str_replace('\\','/', $jx_base_path) . '/');
-}else{
-	die('Incorrect path folder to run the application.');
-}
+define('BASE_PATH', str_replace('\\','/', $jx_base_path) . '/');
 
 
 //
