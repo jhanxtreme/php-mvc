b028ac07902d4b3c406874be6d216e53e1b1b764
jhan mateo <jhanxtreme@gmail.com>
2014-10-13 01:11:43 +0800

    minor changes: enhance folder isolation

--- core/View.php
+++ core/View.php
@@ -7,10 +7,11 @@ interface iView{
 class View implements iView{
 
 	public static function renderHTML($name, $data){
+		$dir = explode("/",	 $name);
 		extract($data);
 		try{
-			if(file_exists(APP_PATH.DS.'application'.DS.'views'.DS.$name.'.tpl'.EXT)){
-				require_once(APP_PATH.DS.'application'.DS.'views'.DS.$name.'.tpl'.EXT);
+			if(file_exists(APP_PATH.DS.'application'.DS.'views'.DS.$dir[0].DS.$dir[1].'.tpl'.EXT)){
+				require_once(APP_PATH.DS.'application'.DS.'views'.DS.$dir[0].DS.$dir[1].'.tpl'.EXT);
 			}else{
 				require_once(APP_PATH.DS.'application'.DS.'views'.DS.'default'.DS.'default.tpl'.EXT);
 			}
